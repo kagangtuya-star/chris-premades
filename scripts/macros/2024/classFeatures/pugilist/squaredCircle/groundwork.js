36b59cf4415ce2c10981d2e905011b0616b4d56d
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-02-21 21:07:06 -0500

    Bugfix - Deprecated Midi Flags (#501)
    * update deprecated midi flags
    
    disadvantage to saving throws
    
    * shouldn't have changed this one

--- scripts/macros/2024/classFeatures/pugilist/squaredCircle/groundwork.js
+++ scripts/macros/2024/classFeatures/pugilist/squaredCircle/groundwork.js
@@ -36,7 +36,7 @@ async function inescapable({trigger: {entity: item}, workflow}) {
         },
         changes: [
             {
-                key: 'flags.midi-qol.disadvantage.ability.save.all',
+                key: 'flags.midi-qol.disadvantage.save.all',
                 value: '1',
                 mode: 5,
                 priority: 120
